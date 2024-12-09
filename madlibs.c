423d2c65c32ebd31498ac9404abc2fbb47088585
1102-Cardinalli-Kenneth <cardinallikenneth@gmail.com>
2024-12-09 18:06:00 +0000

    Update madlibs.c

--- madlibs.c
+++ madlibs.c
@@ -13,33 +13,31 @@
 void stringCopy(char[], char[]);
 void stringSwap(char[], char[]);
 int stringLength(char[]);
-void userPrompt(char[], char[][COLUMN]);
+void userPrompt(char[][COLUMN], char[][COLUMN]);
 //Please put inital next to chosen function, and feel free to change whatever
-void textGrab(FILE *fPtr, char fullText[ROW][COLUMN]); //JS
+void textGrab(FILE* fPtr, char[][COLUMN]); //JS
 void wordSwap();
 
 int main(){
-	char fullText[ROW][COLUMN], swappedText[ROW][COLUMN], letters[11];
+	char fullText[ROW][COLUMN], swappedText[ROW][COLUMN], words[10][COLUMN];
 	
 	FILE* fPtr = fopen(FILE_NAME, "r");
 		if(fPtr == NULL){	
 		printf("Cant open file\n");
 		return 0;
 		}
-
-
-
-
+	textGrab(FILE* fPtr, fullText[ROW][COLUMN]);
 	fclose(fPtr);
+	userPrompt(words[ROW][COLUMN], fullText[ROW][COLUMN]);
+
+ 
+
+	
 return 0;
 }
 
 //Functions
 //Idk if you guys will use this one but if you need it will output the value of the string length
-void textGrab(FILE *fPtr, char fullText[ROW][COLUMN]){
-	fscanf(fPtr, "%c", fullText);
-
-}
 int stringLength(){
 	int counter = 0;
 		for(int index = 0; str[index] != '\0'; index++){
@@ -61,8 +59,33 @@ void stringSwap(char str1[], char str2[]){
 	stringCopy(str2, str1);
 	stringCopy(strCopy, str2);
 }
-void userPrompt(char letters[], char fullText[][COLUMN]){
-	
+void userPrompt(char words[][COLUMN], char fullText[][COLUMN]){
+	int index3 = 0;
+	for(int index = 0; index < ROW; index++){
+		for(int index2 = 0; fullText[index][index2] != 0; index2++){
+			if(ROW % 2 == 0){
+			switch(fullText[index][index2]){
+				case 'A':
+					printf("Please enter an adjective:");
+					scanf("%s", words[index3][COLUMN]);
+					index3 += 1;
+					break;
+				case 'N':
+					printf("Please enter a noun:");
+					scanf("%s", words[index3][COLUMN]);
+					index3 += 1;
+					break;
+				case 'V':
+					printf("Please enter a verb:");
+					scanf("%s", words[index3][COLUMN]);
+					index3 += 1;
+					break;
+				}
+			}
+		}
+	}
+}
+void textGrab(FILE* fPtr, char[][COLUMN]){
 	for(int index = 0; index < ROW; index++){
 		for(int index2 = 0; fullText[index][index2] != 0; index2++){
 		fscanf(fPtr, fullText[index][index2]);
