7a25574a132a11168c7ecfa476ef51308b024369
1102-Cardinalli-Kenneth <cardinallikenneth@gmail.com>
2024-12-11 06:04:26 +0000

    Update madlibs.c

--- madlibs.c
+++ madlibs.c
@@ -5,80 +5,91 @@
 
 #include<stdio.h>
 #define STR_CAP 100
-#define ROW 22
+#define ROW 100
 #define COLUMN 100
-#define FILE_NAME "madlib1.txt"
+#define FILE_NAME "madlib2.txt"
 
-//Function prototypes
+// Function prototypes
 void stringCopy(char[], char[]);
-void stringSwap(char[], char[]);
+void displayMadlib(char fullText[][COLUMN]);
 void userPrompt(char[][COLUMN], char[][COLUMN]);
-//Please put inital next to chosen function, and feel free to change whatever
-void textGrab(FILE* fPtr, char[][COLUMN]); //JS
-void wordSwap();
+void textGrab(FILE* fPtr, char[][COLUMN]);
+void textReplace();
 
-int main(){
-	char fullText[ROW][COLUMN], swappedText[ROW][COLUMN], words[10][COLUMN];
-	
-	FILE* fPtr = fopen(FILE_NAME, "r");
-		if(fPtr == NULL){	
-		printf("Cant open file\n");
-		return 0;
-		}
-	textGrab(fPtr, fullText);
-	fclose(fPtr);
-	userPrompt(words, fullText);
-
- 
-
-	
-return 0;
+int main() {
+    char fullText[ROW][COLUMN], swappedText[ROW][COLUMN], words[10][COLUMN];
+    
+    FILE* fPtr;
+    fPtr = fopen(FILE_NAME, "r");
+    if(fPtr == NULL){    
+        printf("Can't open file\n");
+        return 0;
+    }
+    textGrab(fPtr, fullText);
+    fclose(fPtr);
+    userPrompt(words, fullText);
+    textReplace();
+    displayMadlib(fullText);
+    return 0;
 }
 
-//Functions
-//Idk if you guys will use this one but if you need it will output the value of the string length
-void stringCopy(char source[], char dest[]){
-	for(int index = 0; source[index] != '\0'; index++){
-	dest[index] = source[index];
+// Functions
+void stringCopy(char source[], char dest[]) {
+    for(int index = 0; source[index] != '\0'; index++) {
+        dest[index] = source[index];
+    }
+}
+
+void displayMadlib(char fullText[][COLUMN]){
+	for(int index = 0; index < ROW; index++){
+		printf("%s", fullText[index]);
 	}
 }
-void stringSwap(char str1[], char str2[]){
-	char strCopy[STR_CAP];
-	
-	stringCopy(str1, strCopy);
-	stringCopy(str2, str1);
-	stringCopy(strCopy, str2);
+
+void userPrompt(char words[][COLUMN], char fullText[][COLUMN]) {
+    int index3 = 0;  // Start at the beginning of the words array
+    int count = 0;   // Counter to track how many inputs we've taken
+
+    for(int index = 0; index < ROW; index++) {
+        for(int index2 = 0; fullText[index][index2] != '\0'; index2++) {
+            if (count >= 10) {
+                return;  // Exit after taking 10 inputs
+            }
+            switch(fullText[index][index2]) {
+                case 'A':
+                    printf("Please enter an adjective: ");
+                    scanf("%s", words[index3]);
+                    index3 += 1;
+                    count++;  // Increment count after each input
+                    break;
+                case 'N':
+                    printf("Please enter a noun: ");
+                    scanf("%s", words[index3]);
+                    index3 += 1;
+                    count++;  // Increment count after each input
+                    break;
+                case 'V':
+                    printf("Please enter a verb: ");
+                    scanf("%s", words[index3]);
+                    index3 += 1;
+                    count++;  // Increment count after each input
+                    break;
+            }
+        }
+    }
 }
-void userPrompt(char words[][COLUMN], char fullText[][COLUMN]){
-	int index3 = 0;
-	for(int index = 0; index < ROW; index++){
-		for(int index2 = 0; fullText[index][index2] != 0; index2++){
-			if(ROW % 2 == 0){
-			switch(fullText[index][index2]){
-				case 'A':
-					printf("Please enter an adjective:");
-					scanf("%s", words[index3][COLUMN]);
-					index3 += 1;
-					break;
-				case 'N':
-					printf("Please enter a noun:");
-					scanf("%s", words[index3][COLUMN]);
-					index3 += 1;
-					break;
-				case 'V':
-					printf("Please enter a verb:");
-					scanf("%s", words[index3][COLUMN]);
-					index3 += 1;
-					break;
-				}
-			}
-		}
-	}
+
+void textGrab(FILE* fPtr, char fullText[ROW][COLUMN]) {
+    	int index = 0;
+        while (fscanf(fPtr, "%s", fullText[index]) == 1){  // Read the file into fullText
+    		index++;
+    }
 }
-void textGrab(FILE* fPtr, char fullText[][COLUMN]){
+void textReplace(char fullText[][COLUMN], char words[][COLUMN]){
+	int indexW = 0;
 	for(int index = 0; index < ROW; index++){
-		for(int index2 = 0; fullText[index][index2] != 0; index2++){
-		fscanf(fPtr, "%s", fullText[index][index2]);
+		if(fullText[index] == 'A' || fullText[index] == 'V' || fullText[index] == 'N'){
+		stringCopy();
 		}
 	}
 }
